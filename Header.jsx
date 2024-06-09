103bb5b47e3c36565bea87e6d7a8c5eff95a0534
charans67 <89794217+charans67@users.noreply.github.com>
2024-06-09 11:10:12 +0530

    Update Header.jsx

--- Header.jsx
+++ Header.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
-
+import { BrowserRouter as Router, useNavigate } from "react-router-dom";
 
 function Header() {
+  const navigate = useNavigate();
   return (
     <div className="heading">
     <div className="name">
@@ -10,19 +11,19 @@ function Header() {
      <div>
      <nav>
         
-          <button>Home</button>
-         <button>Shop</button>
-          <button>Shop Detail</button>
-          <button>Pages</button>
-         <button>Contact</button>
+          <button className='header-button' onClick={()=>navigate("/home")} >Home</button>
+         <button className='header-button' onClick={()=>navigate("/shop")}>Shop</button>
+          <button className='header-button'>Shop Detail</button>
+          <button className='header-button'>Pages</button>
+         <button className='header-button'>Contact</button>
         
         </nav>
         </div>
         <div className="search">
         <nav>
-        <button>🔍</button>
-        <button>🛒</button>
-        <button>👤</button>
+        <button  className='header-button'>🔍</button>
+        <button className='header-button'>🛒</button>
+        <button className='header-button'>👤</button>
         </nav>
     </div>
     </div>
